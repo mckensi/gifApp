c2c7f8890c2fb526550567f260f670ae827dd17f
Daniel Steven Murcia Almanza <daniel.steven.murcia@gmail.com>
2022-03-28 13:22:04 -0500

    added a new unit test

---
+++ giphyFreshTests/Services/NetworkManagerMock.swift
@@ -0,0 +1,59 @@
+//
+//  NetworkManagerMock.swift
+//  giphyFreshTests
+//
+//  Created by Daniel Steven Murcia Almanza on 28/03/22.
+//
+
+import Foundation
+import Combine
+import SwiftUI
+@testable import giphyFresh
+
+struct NetworkManagerMock : DataProvider {
+    
+    
+    let dataTrending = [
+        DataTrendingModel(type: nil, id: "1", url: nil, slug: nil, bitlyGIFURL: nil, bitlyURL: nil, embedURL: nil, username: nil, source: nil, title: nil, rating: nil, contentURL: nil, sourceTLD: nil, sourcePostURL: nil, isSticker: nil, importDatetime: nil, trendingDatetime: nil, images: nil, user: nil, analyticsResponsePayload: nil, analytics: nil),
+        DataTrendingModel(type: nil, id: "2", url: nil, slug: nil, bitlyGIFURL: nil, bitlyURL: nil, embedURL: nil, username: nil, source: nil, title: nil, rating: nil, contentURL: nil, sourceTLD: nil, sourcePostURL: nil, isSticker: nil, importDatetime: nil, trendingDatetime: nil, images: nil, user: nil, analyticsResponsePayload: nil, analytics: nil),
+        DataTrendingModel(type: nil, id: "3", url: nil, slug: nil, bitlyGIFURL: nil, bitlyURL: nil, embedURL: nil, username: nil, source: nil, title: nil, rating: nil, contentURL: nil, sourceTLD: nil, sourcePostURL: nil, isSticker: nil, importDatetime: nil, trendingDatetime: nil, images: nil, user: nil, analyticsResponsePayload: nil, analytics: nil)
+    ]
+    
+    func getGifPublisher() -> AnyPublisher<Data, Error> {
+        var data: Data?
+        let trendingRes = TrendingResponseModel(
+            data: dataTrending,
+            pagination: nil,
+            meta: nil
+        )
+        let encoder = JSONEncoder()
+        do {
+            data = try encoder.encode(trendingRes)
+            return Just(data!)
+                .setFailureType(to: Error.self)
+                .eraseToAnyPublisher()
+        } catch {
+            return Just(data!)
+                .setFailureType(to: Error.self)
+                .eraseToAnyPublisher()
+        }
+    }
+    
+    func searchGifs(search: String) -> AnyPublisher<Data, Error> {
+        var data: Data?
+        let trendingRes = TrendingResponseModel(data: [], pagination: nil, meta: nil)
+        let encoder = JSONEncoder()
+        do {
+            data = try encoder.encode(trendingRes)
+            return Just(data!)
+                .setFailureType(to: Error.self)
+                .eraseToAnyPublisher()
+        } catch {
+            return Just(data!)
+                .setFailureType(to: Error.self)
+                .eraseToAnyPublisher()
+        }
+            
+    }
+    
+}
--- giphyFreshTests/giphyFreshTests.swift
+++ giphyFreshTests/giphyFreshTests.swift
@@ -6,31 +6,36 @@
 //
 
 import XCTest
+import Combine
 @testable import giphyFresh
 
 class giphyFreshTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    var subscriptions = Set<AnyCancellable>()
+    
+    override func tearDown() {
+        subscriptions = []
     }
+    
+    func testLoadingGifs() {
+        let mock = NetworkManagerMock()
+        let gifMainViewModel = GifMainViewModel(networkManager: mock)
+        gifMainViewModel.getGifUsingNetworkManager()
+        XCTAssertEqual(gifMainViewModel.gifs.count, 0)
+        
+        let promise = expectation(description: "loading 3 gifs")
+    
+        
+        gifMainViewModel.$gifs.sink { (completion) in
+            XCTFail()
+        } receiveValue: { gifs in
+            if(gifs.count == 3) {
+                promise.fulfill()
+            }
+        }.store(in: &subscriptions)
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+
+        wait(for: [promise]	, timeout: 1)
+
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
